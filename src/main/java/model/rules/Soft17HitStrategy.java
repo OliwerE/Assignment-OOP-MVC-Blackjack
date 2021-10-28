ae5e7ead2c5673368ca568b83a9833991dcb32eb
Oliwer <oe222ez@student.lnu.se>
2021-10-29 01:07:11 +0200

    Add soft 17 rule variant

---
+++ src/main/java/model/rules/Soft17HitStrategy.java
@@ -0,0 +1,39 @@
+package model.rules;
+
+import model.Card;
+import model.Player;
+
+public class Soft17HitStrategy implements HitStrategy {
+  private static final int hitLimit = 17;
+
+  public boolean doHit(Player dealer) {
+    if (dealer.calcScore() == hitLimit) {
+      Iterable<Card> hand = dealer.getHand();
+
+      int aceCount = 0;
+      for (Card c: hand) {
+        if (c.getValue() == Card.Value.Ace) {
+          aceCount++;
+        }
+      }
+
+      if (aceCount > 0) {
+        return true; // Allow one more card
+      }
+    }
+    return dealer.calcScore() < hitLimit;
+  }
+  
+}
+/*
+ * 
+ * Iterable<Card> hand = dealer.getHand();
+ * 
+ * int aceCount = 0; for (Card c: hand) { if (c.getValue() == Card.Value.Ace) {
+ * aceCount++; } }
+ * 
+ * if (aceCount > 0) { // Hantera ace int currentScore = dealer.calcScore();
+ * 
+ * return false; // ÄNDRA!! } else { return dealer.calcScore() < hitLimit; }
+ * 
+ */
